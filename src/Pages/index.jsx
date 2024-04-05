1fc53688bbe835cf946f0cda5bef713ed6cdc7d8
valclassdevelop <developervalclass.com>
2024-04-05 10:55:53 +0700

    fix

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -587,7 +587,7 @@ const Homepage = () => {
                                 <div className="w-full md:w-[60%] flex items-center justify-end space-y-4 md:space-y-0 dark:bg-gray-900">
                                     <div className="relative flex mr-5 items-center">
                                         <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400md:top-0 relative top-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400md:top-0 relative top-3 md:top-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                             </svg>
                                         </div>
